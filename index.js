0b65457c6322a27a2324c60093f91185f249ff64
cblanc <cablanchard@gmail.com>
2015-01-25 23:36:24 +0000

    Server startup

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var config = require(path.join(__dirname, "config/config.js"));
+
+// Configure App
+require(path.join(__dirname, "config/express.js"))(app, config);
+
+// Configure Routes
+require(path.join(__dirname, "config/routes.js"))(app, config);
+
+app.listen(config.port);
+console.log("Listening on port", config.port);
